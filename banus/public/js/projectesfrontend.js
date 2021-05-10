5cf451218fdcc3b50b620c7c605c26920b031722
nozydm <72210893+nozydm@users.noreply.github.com>
2021-05-10 16:16:44 +0200

    projectes finalitzat

--- banus/public/js/projectesfrontend.js
+++ banus/public/js/projectesfrontend.js
@@ -2,10 +2,10 @@ $( document ).ready(function() {
     $(".ver_mas").click(function(){
         if($(this).children().eq(1).is(":hidden")){
             $(this).children().eq(1).attr("hidden", false);
-            $(this).children().first().text("Click para ver menos");
+            $(this).children().first().text("Click para veure menys");
         }else   {
             $(this).children().eq(1).attr("hidden", true);
-            $(this).children().first().text("Click para ver mas");
+            $(this).children().first().text("Click para ver més");
         }
 
     });
